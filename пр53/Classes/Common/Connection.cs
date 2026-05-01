799c93777a988137be3cbe756b4dd606edd81a18
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-01 13:17:08 +0500

    Разработанный класс подключения к БД. Белослудцева

---
+++ пр53/Classes/Common/Connection.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр53.Classes.Common
+{
+    public class Connection
+    {
+        /// <summary> Строка с настройками для подключения
+        public static string config = "server=127.0.0.1;uid=root;pwd=root;database=journal;";
+
+        /// <summary> Открываем соединение
+        public static MySqlConnection OpenConnection()
+        {
+            // Создаём подключение
+            MySqlConnection connection = new MySqlConnection(config);
+            // Открываем подключение
+            connection.Open();
+            // Возвращаем открытое подключение
+            return connection;
+        }
+
+        /// <summary> Выполнение запроса
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            // Выполняем запрос, возвращаем обратно
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        /// <summary> Закрытие соединения
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            // Закрываем подключение
+            connection.Close();
+            // Очищаем пул подключений
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
